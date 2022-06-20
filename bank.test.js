1ad8768e448d6b9a3db2e4e8cfa7d19eb617a65c
S-Spiegl <steven.spiegl@gmail.com>
2022-06-20 15:37:34 +0100

    implemented tests for withdrawal

--- bank.test.js
+++ bank.test.js
@@ -7,33 +7,26 @@ describe('Bank', () => {
   })
 
     describe('.deposit', () => {
-    //   it('adds funds to the account', () => {
-    //     bank.deposit(10)
-    //     expect(bank.printStatement()).toEqual(10)
-    // });
-
-    // it('withdraws funds from the account', () => {
-    //   bank.deposit(10)
-    //   bank.withdraw(10)
-    //   expect(bank.printStatement()).toEqual(0)
-    // });
-
-    // it('stores the deposit, the withdrawal and the balance', () => {
-    //   bank.deposit(10)
-    //   expect(bank.printStatement()).toEqual([10, 0, 10])
-    // })    
-
-    it('augments the account by the amount deposited', () => {
-      bank.deposit(10)
-      bank.deposit(10)
-      expect(bank.account).toEqual([[10, 0], [10, 0]])
-    })
+      it('augments the account by the amount deposited', () => {
+        bank.deposit(10)
+        bank.deposit(10)
+        expect(bank.account).toEqual([[10, 0], [10, 0]])
+      })
 
     describe('.printStatement', () => {
       it('stores two deposits/withdrawals', () => {
         bank.deposit(10)
         bank.deposit(10)
         expect(bank.printStatement()).toEqual([[10, 0, 10], [10, 0, 20]])
+      })
+    })
+
+    describe('.withdraw', () => {
+      it('deceases the account by the amount withdrawn', () => {
+        bank.deposit(10)
+        bank.deposit(10)
+        bank.withdraw(5)
+        expect(bank.printStatement()).toEqual([[10, 0, 10], [10, 0, 20], [0, 5, 15]])
       })
     })
   })
